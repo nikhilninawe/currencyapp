7000e5d16765e759d6c84f70c0af4d2fa28632df
Nikhil Ninawe <nninawe@adadyn.com>
2016-03-14 14:52:16 +0530

    Removing unused files

--- src/main/java/com/currency/config/ApplicationConfig.java
+++ src/main/java/com/currency/config/ApplicationConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.AsyncRestTemplate;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(basePackages = {"com.currency.controller", "com.currency.service"})
